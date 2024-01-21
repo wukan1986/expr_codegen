48389e4eb5dbeca7c94d3b493023e8f8eedc43d8
wukan <wu-kan@163.com>
2024-01-21 19:31:57 +0800

    分析功能已经移动到alphainspect中

--- analysis/demo3.py
+++
@@ -1,9 +0,0 @@
-from analysis.reports import ipynb_to_html
-
-# 从main中导入，可以大大减少代码
-ipynb_to_html('template.ipynb',
-              output='demo.html',
-              no_input=True,
-              no_prompt=False,
-              open_browser=True,
-              TEST1='123456')
