12a2e46b71503bf3013a3b975b50751c5f7dd758
Kan <wu-kan@163.com>
2025-10-14 19:07:51 +0800

    drop模板调整

--- expr_codegen/pandas/code.py
+++ expr_codegen/pandas/code.py
@@ -13,18 +13,23 @@ def get_groupby_from_tuple(tup, func_name, drop_cols):
     """从传入的元组中生成分组运行代码"""
     prefix2, *_ = tup
 
+    if len(drop_cols)>0:
+        drop_str = f'.drop(columns={drop_cols})'
+    else:
+        drop_str = ""
+
     if prefix2 == TS:
         # 组内需要按时间进行排序，需要维持顺序
         prefix2, asset = tup
-        return f'df = df.groupby(by=[_ASSET_], group_keys=False).apply({func_name}).drop(columns={drop_cols})'
+        return f'df = df.groupby(by=[_ASSET_], group_keys=False).apply({func_name}){drop_str}'
     if prefix2 == CS:
         prefix2, date = tup
-        return f'df = df.groupby(by=[_DATE_], group_keys=False).apply({func_name}).drop(columns={drop_cols})'
+        return f'df = df.groupby(by=[_DATE_], group_keys=False).apply({func_name}){drop_str}'
     if prefix2 == GP:
         prefix2, date, group = tup
-        return f'df = df.groupby(by=[_DATE_, "{group}"], group_keys=False).apply({func_name}).drop(columns={drop_cols})'
+        return f'df = df.groupby(by=[_DATE_, "{group}"], group_keys=False).apply({func_name}){drop_str}'
 
-    return f'df = {func_name}(df).drop(columns={drop_cols})'
+    return f'df = {func_name}(df){drop_str}'
 
 
 def symbols_to_code(syms):
--- expr_codegen/polars/code.py
+++ expr_codegen/polars/code.py
@@ -14,18 +14,23 @@ def get_groupby_from_tuple(tup, func_name, drop_cols):
     """从传入的元组中生成分组运行代码"""
     prefix2, *_ = tup
 
+    if len(drop_cols)>0:
+        drop_str = f'.drop(*{drop_cols})'
+    else:
+        drop_str = ""
+
     if prefix2 == TS:
         # 组内需要按时间进行排序，需要维持顺序
         prefix2, asset = tup
-        return f'df = {func_name}(df.sort(_ASSET_, _DATE_)).drop(*{drop_cols})'
+        return f'df = {func_name}(df.sort(_ASSET_, _DATE_)){drop_str}'
     if prefix2 == CS:
         prefix2, date = tup
-        return f'df = {func_name}(df.sort(_DATE_)).drop(*{drop_cols})'
+        return f'df = {func_name}(df.sort(_DATE_)){drop_str}'
     if prefix2 == GP:
         prefix2, date, group = tup
-        return f'df = {func_name}(df.sort(_DATE_, "{group}")).drop(*{drop_cols})'
+        return f'df = {func_name}(df.sort(_DATE_, "{group}")){drop_str}'
 
-    return f'df = {func_name}(df).drop(*{drop_cols})'
+    return f'df = {func_name}(df){drop_str}'
 
 
 def symbols_to_code(syms):
